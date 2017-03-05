87ae5ee7d0e47c5e05da4d040fa9a8d6d5cfda2b
Julien Feuvrier <julien;feuvrier@telecomnancy.net>
2017-03-05 18:51:19 +0100

    Basic sparql query sent to the server.

---
+++ sparql.py
@@ -0,0 +1,20 @@
+import urllib2
+
+# useful variables for the http request
+user='student'
+password='5hoPpeR4'
+url='https://pgxlod.loria.fr/bigdata/namespace/kb/sparql'
+sparqlQuery='query=SELECT * { ?s ?p ?o } LIMIT 1'
+
+# create the http request
+request = urllib2.Request(url)
+request.add_header('Accept', 'application/sparql-results+json')
+request.add_header('Authorization', "Basic " + (user + ":" + password).encode("base64").rstrip())
+
+# make the http request which is equivalent to : 
+# curl --user student:5hoPpeR4 -v -H 'Accept:application/sparql-results+json' -X POST https://pgxlod.loria.fr/bigdata/namespace/kb/sparql --data-urlencode 'query=sparqlQuery'
+result = urllib2.urlopen(url = request, data = sparqlQuery)
+
+# print the results
+print result.read()
+
