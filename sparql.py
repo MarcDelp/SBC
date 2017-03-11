109d89ed9ba745133ee73df89e3166e87367d8b3
Marc <marc.delpech@telecomnancy.net>
2017-03-11 16:07:55 +0100

    Init branch Marc

--- sparql.py
+++ sparql.py
@@ -4,12 +4,17 @@ import urllib2
 user='student'
 password='5hoPpeR4'
 url='https://pgxlod.loria.fr/bigdata/namespace/kb/sparql'
-sparqlQuery='query=SELECT * { ?s ?p ?o } LIMIT 1'
 
 # create the http request
 request = urllib2.Request(url)
 request.add_header('Accept', 'application/sparql-results+json')
 request.add_header('Authorization', "Basic " + (user + ":" + password).encode("base64").rstrip())
+
+
+def create_query(gene, medicine):
+    pass
+
+sparqlQuery='query=SELECT * { ?s ?p ?o } LIMIT 1'
 
 # make the http request which is equivalent to : 
 # curl --user student:5hoPpeR4 -v -H 'Accept:application/sparql-results+json' -X POST https://pgxlod.loria.fr/bigdata/namespace/kb/sparql --data-urlencode 'query=sparqlQuery'
